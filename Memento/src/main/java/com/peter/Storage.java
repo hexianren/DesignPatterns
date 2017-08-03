f33963718e2207d23971cc510ae8e854a31e71c8
Li Heming <liheming@qding.me>
2017-08-03 10:55:54 +0100

    修改备忘录模式

--- Memento/src/main/java/com/peter/Storage.java
+++ Memento/src/main/java/com/peter/Storage.java
@@ -2,22 +2,19 @@ package com.peter;
 
 /**
  * Storage类是存储备忘录的类，持有Memento类的实例，负责保存好备忘录
- * @author Administrator
- *
  */
 public class Storage {  
-    
-    private Memento memento;  
-      
-    public Storage(Memento memento) {  
-        this.memento = memento;  
-    }  
-  
-    public Memento getMemento() {  
-        return memento;  
-    }  
-  
-    public void setMemento(Memento memento) {  
-        this.memento = memento;  
-    }  
+	private Memento memento;  
+
+	public Storage(Memento memento) {  
+		this.memento = memento;  
+	}  
+
+	public Memento getMemento() {  
+		return memento;  
+	}  
+
+	public void setMemento(Memento memento) {  
+		this.memento = memento;  
+	}  
 } 
--- Memento/src/main/java/com/peter/Test.java
+++ Memento/src/main/java/com/peter/Test.java
@@ -2,23 +2,19 @@ package com.peter;
 
 /**
  * 备忘录模式的测试
- * @author Administrator
- * 2014/08/07
  */
 public class Test {  
-	  
-    public static void main(String[] args) {  
-          
-        // 创建原始类  
-        Original origi = new Original("egg");  
-        // 创建备忘录  
-        Storage storage = new Storage(origi.createMemento());  // egg被保存起来
-        // 修改原始类的状态  
-        System.out.println("初始化状态为：" + origi.getValue());  
-        origi.setValue("niu");  
-        System.out.println("修改后的状态为：" + origi.getValue());  
-        // 回复原始类的状态  
-        origi.restoreMemento(storage.getMemento());  
-        System.out.println("恢复后的状态为：" + origi.getValue());  
-    }
+	public static void main(String[] args) {  
+		// 创建原始类  
+		Original origi = new Original("egg");  
+		// 创建备忘录  
+		Storage storage = new Storage(origi.createMemento());  // egg被保存起来
+		// 修改原始类的状态  
+		System.out.println("初始化状态为：" + origi.getValue());  
+		origi.setValue("niu");  
+		System.out.println("修改后的状态为：" + origi.getValue());  
+		// 回复原始类的状态  
+		origi.restoreMemento(storage.getMemento());  
+		System.out.println("恢复后的状态为：" + origi.getValue());  
+	}
 }
