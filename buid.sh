f908a1349dbab6e3e8589b5920df389b08aff6dd
Li Heming <liheming@qding.me>
2017-08-07 12:07:53 +0100

    添加简易编译工具

---
+++ buid.sh
@@ -0,0 +1,99 @@
+cd AbstractFactory/
+gradle -q run
+cd ..
+
+cd Bridge/
+gradle -q run
+cd ..
+
+cd ChainOfResponsibility/
+gradle -q run
+cd ..
+
+cd Composite/
+gradle -q run
+cd ..
+
+cd Facade/
+gradle -q run
+cd ..
+
+cd FlyWeight/
+gradle -q run
+cd ..
+
+cd Iterator/
+gradle -q run
+cd ..
+
+cd Memento/
+gradle -q run
+cd ..
+
+cd Observer/
+gradle -q run
+cd ..
+
+cd ProxyDynamic/
+gradle -q run
+cd ..
+
+cd Singleton/
+gradle -q run
+cd ..
+
+cd Strategy/
+gradle -q run
+cd ..
+
+cd Visitor/
+gradle -q run
+cd ..
+
+cd Adapter/
+gradle -q run
+cd ..
+
+cd Builder/
+gradle -q run
+cd ..
+
+cd Command/
+gradle -q run
+cd ..
+
+cd Decorator/
+gradle -q run
+cd ..
+
+cd Factory/
+gradle -q run
+cd ..
+
+cd Interpreter/
+gradle -q run
+cd ..
+
+cd Mediator/
+gradle -q run
+cd ..
+
+cd MementoWideNarrow/
+gradle -q run
+cd ..
+
+cd Prototype/
+gradle -q run
+cd ..
+
+cd ProxyStatic/
+gradle -q run
+cd ..
+
+cd State/
+gradle -q run
+cd ..
+
+cd TemplateMethod/
+gradle -q run
+cd ..
