7f248497b79a6673fb7eee4310b9c1c26aea6fe4
Peter <liheming1990@163.com>
2019-12-24 04:59:27 +0800

    更新适配器模式

--- Adapter/src/main/java/com/peter/Adaptee.java
+++ Adapter/src/main/java/com/peter/Adaptee.java
@@ -6,7 +6,7 @@ package com.peter;
  *
  */
 public class Adaptee {
-	public void methodOriginal() {  
-        System.out.println("this is original method!");  
-    } 
+	public void methodOriginal() {
+		System.out.println("this is original method!");
+	} 
 }
